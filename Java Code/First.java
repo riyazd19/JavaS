721649d0cf2850f62c270a9d1e416bdf2c65a38c
riyazd19 <riyazd19@gmail.com>
2022-05-08 17:00:58 +0530

    init

--- Java Code/First.java
+++
@@ -1,7 +0,0 @@
-class First {
-    public static void main(String[] args) {
-        System.out.println(" hello world this is nowshad");
-        System.out.println("abcdcddccdccdc");
-        System.out.println("abcd");
-    }
-}
