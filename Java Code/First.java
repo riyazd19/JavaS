9977e35c5e9ecaec422401f67534478f50bff6af
riyazd19 <riyazd19@gmail.com>
2022-05-08 15:23:33 +0530

    name change

--- Java Code/First.java
+++ Java Code/First.java
@@ -1,6 +1,7 @@
 class First {
     public static void main(String[] args) {
-        System.out.println(" hello world");
+        System.out.println(" hello world this is nowshad");
+        System.out.println("abcdcddccdccdc");
         System.out.println("abcd");
     }
 }
